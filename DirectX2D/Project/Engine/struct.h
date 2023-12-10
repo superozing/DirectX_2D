7334e0957982159bb8f3439f3acb557e004680d8
superozing <amazingozing@gmail.com>
2023-12-10 13:32:24 +0900

    vertex, transform 구조체 추가

--- DirectX2D/Project/Engine/struct.h
+++ DirectX2D/Project/Engine/struct.h
@@ -1,1 +1,17 @@
 #pragma once
+
+// Vertex(정점)
+struct Vtx
+{
+	Vec3 vPos;		 // 정점의 좌표	
+	Vec4 vColor;	 // 정점의 색상 정보
+	Vec2 vUV;		 // UV 좌표계 || Texture Coodinate
+};
+
+
+// 상수 버퍼 대응 구조체 (오프셋과 비슷한 개념)
+struct tTransform 
+{
+	Vec4	vWorldPos;
+	Vec4    vWorldScale;
+};
