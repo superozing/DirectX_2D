216f59defce01b3c9b87120243c7bec50e9176d8
superozing <amazingozing@gmail.com>
2023-12-17 22:08:21 +0900

    CComponent 정의
    모든 컴포넌트의 기반이 될 클래스인 CComponent 정의.
    컴포넌트는 반드시 생성자에 컴포넌트 타입을 받도록 구조 설정.
    자식 쪽에서 객체를 생성할 때, 컴포넌트 쪽 생성자에 자신 컴포넌트 타입을 인자로 넣도록 해줄 예정.
    
    begin, tick, finaltick, updatedata 추가.
    tick, finaltick, updatedata는 매 틱 시점마다 호출 됨.
    tick은 매 틱마다 수행해야 할 동작
    finaltick은 매 틱을 수행하고 결과를 최종적으로 수정하는 동작
    updatedata는 수행된 결과를 GPU메모리로 보내는 동작.
    게임오브젝트 또는 렌더 컴포넌트의 render가 호출되기 직전에 세팅한다.

--- DirectX2D/Project/Engine/CComponent.cpp
+++ DirectX2D/Project/Engine/CComponent.cpp
@@ -1,2 +1,14 @@
 #include "pch.h"
 #include "CComponent.h"
+
+
+
+// 기본 생성자를 정의하지 않음. 반드시 컴포넌트 타입을 인자로 받도록
+CComponent::CComponent(COMPONENT_TYPE _type)
+	:m_Type(_type)
+{
+}
+
+CComponent::~CComponent()
+{
+}
--- DirectX2D/Project/Engine/CComponent.h
+++ DirectX2D/Project/Engine/CComponent.h
@@ -1,7 +1,30 @@
 #pragma once
 #include "CEntity.h"
+
+class CGameObject;
+
 class CComponent :
     public CEntity
 {
+private:
+    const COMPONENT_TYPE    m_Type;     // 컴포넌트 타입을 가지고 있으면 나중에 dynamic_cast를 사용하지 않아도 바로 강제 형변환이 가능하다.
+    CGameObject*            m_Owner;    // 해당 컴포넌트를 소유하는 게임오브젝트 포인터. 쌍방으로 가리킴
+
+
+public:
+    virtual void begin() {}             // 순수 가상 함수는 아니지만 상속받아 사용할 수 있도록 만들기 위해.
+    virtual void tick() {}
+    virtual void finaltick() = 0; // finaltick()은 최종 fix 작업. 컴포넌트 들이 해야 할 일을 보통 finaltick()에 구현 예정
+    virtual void UpdateData() {}  // GPU 버퍼로 데이터 보내기 를 의미하는 함수
+
+public:
+    COMPONENT_TYPE GetType() const { return m_Type; }
+
+
+public:
+    CComponent(COMPONENT_TYPE _type);
+    ~CComponent();
+
+    friend class CGameObject;
 };
 
