9022ed26a0ddba67ded8400632c4aa38b1488c59
superozing <amazingozing@gmail.com>
2023-12-18 10:01:04 +0900

    meshrender component 추가
    첫 번째 render component
    
    매쉬(정점 + 인덱스) 정보를 통한 render에 사용할 예정

---
+++ DirectX2D/Project/Engine/CMeshRender.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+#include "CMeshRender.h"
+
+CMeshRender::CMeshRender()
+	: CRenderComponent(COMPONENT_TYPE::MESHRENDER)
+{
+}
+
+CMeshRender::~CMeshRender()
+{
+}
---
+++ DirectX2D/Project/Engine/CMeshRender.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "CRenderComponent.h"
+class CMeshRender :
+    public CRenderComponent
+{
+private:
+
+public:
+    CMeshRender();
+    ~CMeshRender();
+};
+
