0eac72112877f8574768a7f292bfc3854f0261b8
superozing <amazingozing@gmail.com>
2023-12-19 18:40:28 +0900

    component.h 헤더 추가
    모든 컴포넌트를 모아놓아서 참조를 편하게 할 수 있도록 해줌.

---
+++ DirectX2D/Project/Engine/components.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// 모든 컴포넌트를 모아놓아서 참조를 편하게 할 수 있도록 해줌.
+
+#include "CTransform.h"
+#include "CMeshRender.h"
+//#include "CCamera.h"
+
+//#include "CCollider2D.h"
+//#include "CCollider3D.h"
+
+//#include "CAnimator2D.h"
+//#include "CAnimator3D.h"
+//
+//#include "CLight2D.h"
+//#include "CLight3D.h"
+//
+//#include "Skybox.h"
+//#include "Decal.h"
+//#include "Landscape.h"
+//#include "ParticleSystem.h"
