65a300652f947db7f4f0beb3c6d2ead3dbcaf682
superozing <amazingozing@gmail.com>
2023-12-19 18:26:50 +0900

    게임오브젝트에 tick, finaltick, render 추가

--- DirectX2D/Project/Engine/CGameObject.cpp
+++ DirectX2D/Project/Engine/CGameObject.cpp
@@ -1,11 +1,60 @@
 #include "pch.h"
 #include "CGameObject.h"
 
+#include "CComponent.h"
+
 CGameObject::CGameObject()
-	:m_arrCom{}
+	: m_arrCom{}
 {
 }
 
 CGameObject::~CGameObject()
 {
+	Delete_Array(m_arrCom);
+}
+
+void CGameObject::begin()
+{
+	for (UINT i = 0; i < UINT(COMPONENT_TYPE::END); ++i)
+	{
+		if (nullptr != m_arrCom[i])
+		{
+			m_arrCom[i]->begin();
+		}
+	}
+}
+
+void CGameObject::tick()
+{
+	for (UINT i = 0; i < UINT(COMPONENT_TYPE::END); ++i)
+	{
+		if (nullptr != m_arrCom[i])
+		{
+			m_arrCom[i]->tick();
+		}
+	}
+}
+
+void CGameObject::finaltick()
+{
+	for (UINT i = 0; i < UINT(COMPONENT_TYPE::END); ++i)
+	{
+		if (nullptr != m_arrCom[i])
+		{
+			m_arrCom[i]->finaltick();
+		}
+	}
+}
+
+void CGameObject::render()
+{
+
+}
+
+void CGameObject::AddComponent(CComponent* _Comonent)
+{
+	COMPONENT_TYPE type = _Comonent->GetType();
+
+	m_arrCom[(UINT)type] = _Comonent;
+	_Comonent->m_Owner = this;
 }
--- DirectX2D/Project/Engine/CGameObject.h
+++ DirectX2D/Project/Engine/CGameObject.h
@@ -7,8 +7,24 @@ class CGameObject :
     public CEntity
 {
 private:
-    // 이제 모든 오브젝트는 컴포넌트를 배열로 들고 있는다.
-    CComponent* m_arrCom[(UINT)COMPONENT_TYPE::END]; 
+    // 게임 오브젝트는 컴포넌트를 
+    CComponent*     m_arrCom[(UINT)COMPONENT_TYPE::END];
+
+
+public:
+    void begin();
+
+    void tick();
+
+    void finaltick();
+
+    void render();
+
+
+
+public:
+    void AddComponent(CComponent* _Comonent);
+
 
 public:
     CGameObject();
