af3af36e9980ca2d4fb3de4f7b5e3c8c883e1575
superozing <amazingozing@gmail.com>
2023-12-20 10:25:03 +0900

    CPlayerScript 추가
    플레이어가 움직일 수 있는 기능을 제공하는 플레이어 스크립트 추가중

---
+++ DirectX2D/Project/Engine/CPlayerScript.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "CScript.h"
+
+// 키 입력에 따라 플레이어 움직임 기능
+class CPlayerScript :
+    public CScript
+{
+private:
+    float m_Speed;
+
+public:
+    virtual void tick() override;
+
+};
+
